053fa94cc0452e804c313eb732b1e876ef5513d4
lyz <360195045@qq.com>
2016-07-27 19:15:11 +0800

    1.0

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/5/8
+ * Time: 21:08
+ */
+namespace App\Http\Controllers;
+use App\libs\common;
+class IndexController extends Controller{
+    public function index(){
+        define("TOKEN", "weixin");
+        $wechatObj = new wechatCallbackapiTest();
+        $wechatObj->valid();
+        $arr=new wechatCallbackapiTest();
+        $obj=$arr->responseMsg();
+        echo $obj;
+    }
+}
