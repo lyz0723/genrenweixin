d892df9ca6d73c72437bac8893d1e48f3b27a148
lyz <360195045@qq.com>
2016-07-27 20:03:25 +0800

    1.9

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,8 +6,7 @@
  * Time: 21:08
  */
 namespace App\Http\Controllers;
-use App\libs\common;
-
+use App\Libs\common;
 class IndexController extends Controller{
     public function index(){
         //define("TOKEN", "weixin");
