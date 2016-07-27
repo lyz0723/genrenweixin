f0983e78f10157e68900c50982dc46ca2669664b
lyz <360195045@qq.com>
2016-07-27 21:08:42 +0800

    2.1

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers;
 use App\Libs\wechatCallbackapiTest;
 class IndexController extends Controller{
     public function index(){
-        //define("TOKEN", "weixin");
+        define("TOKEN", "weixin");
         $wechatObj = new wechatCallbackapiTest();
-        $obj=$wechatObj->valid();
-        echo $obj;
+        $wechatObj->valid();
+
     }
 }
