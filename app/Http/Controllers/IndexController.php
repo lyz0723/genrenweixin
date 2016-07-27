47bb7671caf9562c984bc68d73dd6c176c6c4b1b
lyz <360195045@qq.com>
2016-07-27 21:42:58 +0800

    2.1

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -1,26 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Libs\wechatCallbackapiTest;
 class IndexController extends Controller{
     public function index(){
-        // 1.将timestamp， nonce，token 按字典排序
-        $timestamp = $_GET['timestamp'];
-        $nonce     = $_GET['nonce'];
-        $token     = 'weixin';
-        $signature = $_GET['signature'];
-        $array     = array( $timestamp, $nonce, $token );
-        sort( $array );
-        // 2.将排序后的三个参数拼接之后用sha1加密
-        $tmpstr    = implode('', $array);
-        $tmpstr    = sha1( $tmpstr );
-        $echoStr = $_GET["echostr"];
-        // 3.将加密后的字符串与signature进行对比，判断该请求是否来自微信
-        if ( $tmpstr == $signature && $echoStr ) {
-            echo $_GET['echostr'];
-            exit;
-        }else {
-            $this -> reponseMeg();
-        }
+        define("TOKEN", "weixin");
+        $wechatObj = new wechatCallbackapiTest();
+        $wechatObj->valid();
     }
 }
