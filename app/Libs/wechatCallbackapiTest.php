6927ddb49778c77828505a34517119fb7c8a1dd9
lyz <360195045@qq.com>
2016-07-28 08:05:21 +0800

    1.3

--- app/Libs/wechatCallbackapiTest.php
+++ app/Libs/wechatCallbackapiTest.php
@@ -1,6 +1,6 @@
 <?php
 namespace App\Libs;
-
+use Request;
 class wechatCallbackapiTest
 {
 	public function valid()
@@ -18,7 +18,7 @@ class wechatCallbackapiTest
     public function responseMsg()
     {
 		//get post data, May be due to the different environments
-        $postStr = file_get_contents("php://input");
+		$postStr = Request::getContent();
       	//extract post data
 		if (!empty($postStr)){
                 /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
