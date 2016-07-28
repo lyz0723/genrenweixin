304de6314f3a052daa1933be1cda79a30faed0e1
lyz <360195045@qq.com>
2016-07-28 12:22:22 +0800

    2.4

--- app/Libs/wechatCallbackapiTest.php
+++ app/Libs/wechatCallbackapiTest.php
@@ -5,7 +5,7 @@ class wechatCallbackapiTest
 {
 	public function valid()
     {
-               $echoStr = $_GET["echostr"];
+        $echoStr = $_GET["echostr"];
         //valid signature , option
         if($this->checkSignature()){
         	echo $echoStr;
@@ -16,7 +16,7 @@ class wechatCallbackapiTest
     public function responseMsg()
     {
 		//get post data, May be due to the different environments
-		$postStr = file_get_contents('php://input');
+		$postStr = $GLOBALS['HTTP_RAW_POST_DATA'];
       	//extract post data
         if (!empty($postStr)){
             /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
