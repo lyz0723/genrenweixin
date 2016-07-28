5876b86a7558a8da0494af7a208249ad95cd0ba3
lyz <360195045@qq.com>
2016-07-28 18:53:13 +0800

    1.0

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -23,7 +23,7 @@ class WeixinController extends Controller
     public function responseMsg()
     {
         //get post data, May be due to the different environments
-        $postStr = $GLOBALS['HTTP_RAW_POST_DATA'];
+        $postStr=file_get_contents('php://input');
         //extract post data
         if (!empty($postStr)){
             /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
